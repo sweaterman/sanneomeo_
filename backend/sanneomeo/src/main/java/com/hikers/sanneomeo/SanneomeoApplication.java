c152e865db9bf232f57a5930b0a4408c90ef97a3
yunsuhee <suheeyun97@gmail.com>
2023-03-06 11:02:03 +0900

    backend 폻더 추가

---
+++ backend/sanneomeo/src/main/java/com/hikers/sanneomeo/SanneomeoApplication.java
@@ -0,0 +1,13 @@
+package com.hikers.sanneomeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanneomeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanneomeoApplication.class, args);
+	}
+
+}
---
+++ backend/sanneomeo/src/test/java/com/hikers/sanneomeo/SanneomeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hikers.sanneomeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanneomeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
