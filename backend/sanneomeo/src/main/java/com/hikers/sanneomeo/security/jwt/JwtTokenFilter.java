37a6e8146edc67056413a8930b92dbbf56bad494
thanku1598 <thanku1598@gmail.com>
2023-03-15 17:55:40 +0900

    Feat: jwt token filter 클래스 복붙
    Former-commit-id: 43dcdf0ceb04fddd78d1b04b93d53cb1ae424811

---
+++ backend/sanneomeo/src/main/java/com/hikers/sanneomeo/security/jwt/JwtTokenFilter.java
@@ -0,0 +1,48 @@
+package com.hikers.sanneomeo.security.jwt;
+
+import com.project.baggu.exception.BaseException;
+import com.project.baggu.exception.BaseResponseStatus;
+import com.project.baggu.utils.JwtTokenUtils;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+@Component
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private final String excludeUrl = "/user/login/**";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        //login 요청의 경우 다음 필터로
+        if ((request.getRequestURI()).equals(excludeUrl)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try{
+            String jwt = JwtTokenUtils.resolveAccessToken(request);
+            //유효할 경우
+            if(JwtTokenUtils.isValidToken(jwt)){
+                Authentication authentication = jwtTokenProvider.getAuthentication(jwt); // 정상 토큰이면 SecurityContext 저장
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else{ //만료된 경우
+                throw new BaseException(BaseResponseStatus.ACCESS_TOKEN_EXPIRED);
+            }
+        } catch (BaseException e){
+            request.setAttribute("exception", e.getStatus().name());
+        }
+        doFilter(request, response, filterChain);
+    }
+}
