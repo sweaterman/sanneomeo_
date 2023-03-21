963a6a2e5d197c898da8db2234f457805dcc053d
김지현 <rla0347@naver.com>
2023-03-21 16:55:28 +0900

    Feat : MainController 생성
    Former-commit-id: f32290f55aa3807d9382852e36806543bb099d5b

---
+++ backend/sanneomeo/src/main/java/com/hikers/sanneomeo/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.hikers.sanneomeo.controller;
+
+import io.swagger.annotations.ApiOperation;
+import net.bytebuddy.build.Plugin;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main")
+public class MainController {
+    @GetMapping("weather/location")
+    public ResponseEntity<?> getWeekWeather(@RequestParam int x, @RequestParam int y) {
+        return null;
+    }
+}
