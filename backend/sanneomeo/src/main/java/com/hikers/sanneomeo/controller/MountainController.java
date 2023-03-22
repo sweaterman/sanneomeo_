efdd8556ea081f5a53f32b61c3fc3754b83e60e5
yunsuhee <suheeyun97@gmail.com>
2023-03-22 14:38:16 +0900

    Fix: 유저 seq 확인
    Former-commit-id: 93cf35ec5cbf438f9400ef4dac95eaad4274c327

--- backend/sanneomeo/src/main/java/com/hikers/sanneomeo/controller/MountainController.java
+++ backend/sanneomeo/src/main/java/com/hikers/sanneomeo/controller/MountainController.java
@@ -54,13 +54,20 @@ public class MountainController {
     @PostMapping("/review")
     public BaseResponseDto<?> writeReview(@RequestBody WriteReviewRequestDto writeReviewRequestDto){
         try{
+
+            //요청 내부의 userSeq와 인증된 userSeq가 다를 경우
+            Long authUserSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
+            if (authUserSeq != writeReviewRequestDto.getUserSeq()) {
+                throw new BaseException(UNAUTHORIZED_USER);
+            }
+
             boolean result = mountainService.writeReview(writeReviewRequestDto);
             return new BaseResponseDto<>(result);
         }catch (Exception e){
             if(e instanceof BaseException){
                 throw e;
             }else{
-                //return new BaseResponseDto<>(BaseResponseStatus.FAIL);
+
                 throw new BaseException(BaseResponseStatus.FAIL);
             }
         }
