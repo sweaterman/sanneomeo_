f4372e03111e2e2e0c5ecfb549bf6084a7312169
이연희 <leeuh7262@naver.com>
2023-03-22 14:24:44 +0900

    Feat: 모바일, 웹 페이지 개발을 위한 반응형 그리드 시스템 코드 추가
    Former-commit-id: 93e38497ef8af4926f056a4ce356e66b74367b6d

--- frontend/overmountain/src/components/common/Card.tsx
+++ frontend/overmountain/src/components/common/Card.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+// 이미지와 정보의 조합
 function Card() {
-  return <></>;
+  return (
+    <>
+      <h3>card 컴포넌트임다.</h3>
+      <div className="container"></div>
+    </>
+  );
 }
 
 export default Card;
--- frontend/overmountain/src/pages/mountain/MountainDetail.tsx
+++ frontend/overmountain/src/pages/mountain/MountainDetail.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Card from '@components/common/Card';
 
 function MountainDetail() {
-  return <></>;
+  return (
+    <>
+      <h1>Mountain Detail 페이지임다</h1>
+      <Card />
+    </>
+  );
 }
 
 export default MountainDetail;
