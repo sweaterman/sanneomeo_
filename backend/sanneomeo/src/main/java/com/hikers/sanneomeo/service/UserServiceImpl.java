3ee28248bee0e2ef774a00ea12b22a71809b29ca
thanku1598 <thanku1598@gmail.com>
2023-03-22 16:02:58 +0900

    Feature: 사진 공개 여부 수정 기능 api 구현
    Former-commit-id: 60343bf34def1b2e30d17c51017823284def429d

---
+++ backend/sanneomeo/src/main/java/com/hikers/sanneomeo/service/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.hikers.sanneomeo.service;
+
+import com.hikers.sanneomeo.domain.RecordPhoto;
+import com.hikers.sanneomeo.exception.BaseException;
+import com.hikers.sanneomeo.exception.BaseResponseStatus;
+import com.hikers.sanneomeo.repository.RecordPhotoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService{
+
+  @Autowired
+  RecordPhotoRepository recordPhotoRepository;
+
+  @Override
+  public boolean updatePhotoStatus(Long authUserSeq, Long photoSeq) {
+
+    //photoSeq에 맞는 recordPhoto가 없을 경우 exception
+    RecordPhoto recordPhoto = recordPhotoRepository.findById(photoSeq).orElseThrow(()-> new BaseException(
+        BaseResponseStatus.FAIL));
+
+    //수정을 요청한 userSeq와 해당 recordPhoto를 등록한 userSeq가 다를 경우 권한 exception
+    if(recordPhoto.getUserSeq()!=authUserSeq) throw new BaseException(BaseResponseStatus.FAIL);
+
+    //수정한다
+    recordPhoto.updateIsPublic();
+
+    return true;
+  }
+}
