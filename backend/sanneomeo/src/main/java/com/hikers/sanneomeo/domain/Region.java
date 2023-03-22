7fa5f54d3482cbdfdc5ed7912603e3ffcd9494f3
김지현 <rla0347@naver.com>
2023-03-22 14:48:32 +0900

    Fix : Region entity 수정
    Former-commit-id: 139a62a82eb839c1b3b4b7f58962976bf52efad7

--- backend/sanneomeo/src/main/java/com/hikers/sanneomeo/domain/Region.java
+++ backend/sanneomeo/src/main/java/com/hikers/sanneomeo/domain/Region.java
@@ -5,11 +5,25 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
 @Getter
-@Setter
-@AllArgsConstructor
+@Table(name = "tbl_region")
 @NoArgsConstructor
 public class Region {
-    private int nx;
-    private int ny;
+    @Id
+    @Column(name = "region_seq")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long regionSeq;
+    @Column(name = "level1")
+    private String level1;
+    @Column(name = "level2")
+    private String level2;
+    @Column(name = "level3")
+    private String level3;
+    @Column(name = "nx")
+    private Integer nx;
+    @Column(name = "ny")
+    private Integer ny;
 }
