5dadaa13e5fcdc14528ec1da9f86d04f3858fbf7
yunsuhee <suheeyun97@gmail.com>
2023-03-22 12:33:36 +0900

    Feat: merge 충돌 해결
    Former-commit-id: 38bae885f593a6b615aafb9ec7362411dc8b851c

--- backend/sanneomeo/src/main/java/com/hikers/sanneomeo/service/MountainService.java
+++ backend/sanneomeo/src/main/java/com/hikers/sanneomeo/service/MountainService.java
@@ -1,10 +1,14 @@
 package com.hikers.sanneomeo.service;
 
+import com.hikers.sanneomeo.dto.request.UploadImagesRequestDto;
 import com.hikers.sanneomeo.dto.response.MountainPosResponseDto;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MountainService {
 
     Optional<MountainPosResponseDto> getPos(String mountainIdx);
+
+    boolean createRecordPhotos(UploadImagesRequestDto uploadImagesRequestDto, List<String> uploadImgUrls, Long mountainSeq);
 }
--- backend/sanneomeo/src/main/java/com/hikers/sanneomeo/service/MountainServiceImpl.java
+++ backend/sanneomeo/src/main/java/com/hikers/sanneomeo/service/MountainServiceImpl.java
@@ -1,12 +1,24 @@
 package com.hikers.sanneomeo.service;
 
 import com.hikers.sanneomeo.domain.Mountain;
+import com.hikers.sanneomeo.domain.RecordPhoto;
+import com.hikers.sanneomeo.dto.request.UploadImagesRequestDto;
 import com.hikers.sanneomeo.dto.response.MountainPosResponseDto;
 import com.hikers.sanneomeo.repository.MountainRepository;
+import com.hikers.sanneomeo.repository.RecordPhotoRepository;
+import com.hikers.sanneomeo.domain.RecordPhoto;
+import com.hikers.sanneomeo.dto.request.UploadImagesRequestDto;
+import com.hikers.sanneomeo.repository.MountainRepository;
+import com.hikers.sanneomeo.repository.RecordPhotoRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,12 +27,37 @@ public class MountainServiceImpl implements MountainService{
     @Autowired
     private MountainRepository mountainRepository;
 
+    @Autowired
+    private RecordPhotoRepository recordPhotoRepository;
+
     @Override
-        public Optional<MountainPosResponseDto> getPos(String mountainIdx) {
+    public Optional<MountainPosResponseDto> getPos(String mountainIdx) {
         //산정보
         Mountain mountain = mountainRepository.findByMountainSeq(mountainIdx).get();
         MountainPosResponseDto mountainPosResponseDto = new MountainPosResponseDto(mountain.getName(),mountain.getLatitude(),mountain.getLongitude(),mountain.getLatitude(),mountain.getDifficulty());
 
         return Optional.of(mountainPosResponseDto);
     }
+
+    @Transactional
+    public boolean createRecordPhotos(UploadImagesRequestDto uploadImagesRequestDto, List<String> uploadImgUrls, Long mountainSeq){
+        uploadImgUrls.stream().forEach(
+                (uploadImgUrl) -> {
+                    RecordPhoto recordPhoto = RecordPhoto.builder()
+                            .mountainSeq(mountainSeq)
+                            .image(uploadImgUrl)
+                            .longitude(uploadImagesRequestDto.getLongitude())
+                            .latitude(uploadImagesRequestDto.getLatitude())
+                            .userSeq(uploadImagesRequestDto.getUserSeq())
+                            .build();
+
+                    recordPhotoRepository.save(recordPhoto);
+                }
+        );
+
+        return true;
+    }
 }
+
+
+
