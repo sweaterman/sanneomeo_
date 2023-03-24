6f5d8389b20311133b18100351d84a2d03d53161
yunsuhee <suheeyun97@gmail.com>
2023-03-24 14:56:56 +0900

    Refactor : 출력문 지움
    Former-commit-id: 08204b9c4e515cd655fb23c84a5acc9267a38d4f

--- backend/sanneomeo/src/main/java/com/hikers/sanneomeo/service/UserServiceImpl.java
+++ backend/sanneomeo/src/main/java/com/hikers/sanneomeo/service/UserServiceImpl.java
@@ -33,7 +33,6 @@ public class UserServiceImpl implements UserService {
         List<ChallengeResponseDto> challengeList = userRepository.challengeistNotMember();
         long conquerNo = 0;
         //로그인 유저가 있을때
-        System.out.println("ddddd"+SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         if (SecurityContextHolder.getContext().getAuthentication().getPrincipal() != "anonymousUser") {
             Long authUserSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
 
