134f7131219448bdd989b531b277529cb7a97af9
이연희 <leeuh7262@naver.com>
2023-03-26 16:25:03 +0900

    Feat: 계절산리스트 API 연동 완료
    Former-commit-id: 878ba5eded778fce880589dff2127202dbca17ab

--- frontend/overmountain/src/components/main/MountainItems.tsx
+++ frontend/overmountain/src/components/main/MountainItems.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {
+  getSeasonMountains,
+  seasonMountains,
+} from '@features/mountain/seasonMountainSlice';
+import { useAppSelector, useAppDispatch } from '@app/hooks';
 import sample from '@assets/images/mountsample.png';
 
 function MountainItems() {
+  // 콘솔에서만 API 테스트 완료
+  const data = useAppSelector(seasonMountains);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(getSeasonMountains());
+  }, [dispatch]);
+
   return (
     <div className="mountain-items">
       <img src={sample} alt="도봉산" />
       <div className="mountain-name">도봉산</div>
+
+      <div>
+        <h4>계절산테스트</h4>
+        <h1>계절: {data.result.season}</h1>
+      </div>
     </div>
   );
 }
