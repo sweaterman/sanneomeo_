90519dc1981205960df749dad1d753adf96ea2b2
김지현 <rla0347@naver.com>
2023-03-28 21:29:37 +0900

    Feat : 주간날씨 api
    Former-commit-id: 05a11eaebb23acc72ce67fcbb1b74ce0f892a784

--- backend/sanneomeo/src/main/java/com/hikers/sanneomeo/controller/MainController.java
+++ backend/sanneomeo/src/main/java/com/hikers/sanneomeo/controller/MainController.java
@@ -1,62 +1,50 @@
 package com.hikers.sanneomeo.controller;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.hikers.sanneomeo.config.YmlConfig;
 import com.hikers.sanneomeo.dto.response.BaseResponseDto;
+import com.hikers.sanneomeo.dto.response.WeatherResponseDto;
 import com.hikers.sanneomeo.exception.BaseException;
-<<<<<<< HEAD
-import org.springframework.beans.factory.annotation.Autowired;
-=======
 import com.hikers.sanneomeo.exception.BaseResponseStatus;
 import com.hikers.sanneomeo.service.MainService;
-import io.swagger.annotations.ApiOperation;
-import net.bytebuddy.build.Plugin;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
->>>>>>> 6592214684e4e625bcadfcf33ec83fdc70d16855
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.spring.web.json.Json;
 
-<<<<<<< HEAD
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
-=======
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
->>>>>>> 6592214684e4e625bcadfcf33ec83fdc70d16855
 
 @RestController
 @RequestMapping("/main")
 public class MainController {
-<<<<<<< HEAD
     @Autowired
     private YmlConfig ymlConfig;
-=======
 
     @Autowired
     private MainService mainService;
 
     @GetMapping("weather/location")
-    public BaseResponseDto<?> getWeekWeather(@RequestParam String lat, @RequestParam String lon) {
->>>>>>> 6592214684e4e625bcadfcf33ec83fdc70d16855
-
-    @GetMapping("weather/location")
-    public BaseResponseDto<String> getWeekWeather(@RequestParam String lat, @RequestParam String lon) {
+    public BaseResponseDto<List<WeatherResponseDto>> getWeekWeather(@RequestParam String lat, @RequestParam String lon) {
         String result = "";
 
         String appid = ymlConfig.getWeatherServiceKey();
         String url_path = ymlConfig.getWeatherEndPoint(); // http://api.openweathermap.org/data/2.5/forecast ? lat & lon & appid
-        StringBuilder sb = new StringBuilder();
-        sb.append(url_path).append("?").append("lat=").append(lat).append("&lon=").append(lon).append("&appid=").append(appid)
-                .append("&units=metric&lang=kr"); // 섭씨온도, korean
-        url_path = sb.toString();
+        String sb = url_path + "?" + "lat=" + lat + "&lon=" + lon + "&appid=" + appid +
+                "&units=metric&lang=kr"; // 섭씨온도, korean
+        url_path = sb;
         System.out.println(url_path);
-
         try {
             URL url = new URL(url_path);
             BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
@@ -64,18 +52,48 @@ public class MainController {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
         // responseDTO 에 담아야함
-        return new BaseResponseDto<>(result);
+        JsonObject jsonObj = (JsonObject) JsonParser.parseString(result);
+        JsonArray weatherArray = (JsonArray) jsonObj.get("list"); // 3시간 간격의 5일치 데이터 : 총 40개
+
+        String[] dayOfWeek = {"일", "월", "화", "수", "목", "금", "토"};
+        LocalDate now = LocalDate.now(); // api 요청 시간 기준으로 5일치 날짜 response
+        int dayOfWeekValue = now.getDayOfWeek().getValue();
+        int dayOfMonth = now.getDayOfMonth();
+        List<WeatherResponseDto> weeklyWeather = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            WeatherResponseDto dayDto = new WeatherResponseDto(dayOfWeek[(dayOfWeekValue + i) % 7], dayOfMonth + i); // 객체 생성
+            JsonObject dayWeather = (JsonObject) weatherArray.get(i * 8); // 하루 초기화
+            JsonObject mainObj = (JsonObject) dayWeather.get("main"); // main에 3시간 기준의 min, max 온도 있음
+            double minOfDay = Double.parseDouble(String.valueOf(mainObj.get("temp_min")));
+            double maxOfDay = Double.parseDouble(String.valueOf(mainObj.get("temp_max")));
+            for(int j=1; j<8; j++) { // 하루의 최저온도 최대온도 계산
+                JsonObject obj = (JsonObject) weatherArray.get(i * 8 + j);
+                JsonObject main = (JsonObject) dayWeather.get("main");
+                double min = Double.parseDouble(String.valueOf(mainObj.get("temp_min")));
+                if(min < minOfDay) minOfDay = min;
+                double max = Double.parseDouble(String.valueOf(mainObj.get("temp_max")));
+                if(max > maxOfDay) maxOfDay = max;
+            }
+            dayDto.setMin(minOfDay);
+            dayDto.setMax(maxOfDay);
+            JsonArray weather = (JsonArray) dayWeather.get("weather");
+            JsonObject weatherObj = (JsonObject) weather.get(0);
+            dayDto.setDescription(weatherObj.get("description").getAsString());
+            weeklyWeather.add(dayDto);
+        }
+        return new BaseResponseDto<>(weeklyWeather);
     }
 
     @GetMapping("/season")
-    public BaseResponseDto<Map<String,Object>> seasonList(){
-        try{
+    public BaseResponseDto<Map<String, Object>> seasonList() {
+        try {
             return new BaseResponseDto<>(mainService.seasonInfo());
-        }catch (Exception e){
-            if(e instanceof BaseException){
+        } catch (Exception e) {
+            if (e instanceof BaseException) {
                 throw e;
-            }else{
+            } else {
                 throw new BaseException(BaseResponseStatus.FAIL);
             }
         }
--- backend/sanneomeo/src/main/java/com/hikers/sanneomeo/dto/response/WeatherResponseDto.java
+++ backend/sanneomeo/src/main/java/com/hikers/sanneomeo/dto/response/WeatherResponseDto.java
@@ -1,11 +1,20 @@
 package com.hikers.sanneomeo.dto.response;
 
-import java.time.DayOfWeek;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class WeatherResponseDto {
-    private String day; // 요일
-    private int dd; // 날짜
-    private int min;
-    private int max;
-    private String description;
+
+    public WeatherResponseDto(String dayOfWeek, int dd) {
+        this.dayOfWeek = dayOfWeek;
+        this.dayOfMonth = dd;
+    }
+
+    private String dayOfWeek; // 요일
+    private int dayOfMonth; // 날짜
+    private double min; // 최저온도
+    private double max; // 최대온도
+    private String description; // 날씨설명
 }
