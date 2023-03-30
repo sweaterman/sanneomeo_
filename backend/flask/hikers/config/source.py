219c926a147bc9f69fb76cb312f80930b5a1de12
yunsuhee <suheeyun97@gmail.com>
2023-03-30 17:33:45 +0900

    Feat : 파일 삭제
    Former-commit-id: 8fa2b552d9544a0a90eab412176eb284e6666e67

--- backend/flask/hikers/config/source.py
+++
@@ -1,8 +0,0 @@
-connection_data = {
-    "database":"mysql+mysqlconnector", # mariadb+mariadbconnector
-    "dbname": "sanneomeo",
-    "user": "ssafy",
-    "password": "ssafy",
-    "host": "localhost",
-    "port" : "3306",
-}
