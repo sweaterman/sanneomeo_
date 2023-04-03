b3dc351394b63302b03b876c6a95366b6e1affbc
이연희 <leeuh7262@naver.com>
2023-04-03 11:24:40 +0900

    Feat: 후기 API 연결 작업 진행 중
    Former-commit-id: 4de76e204c1a77706b9ffd9b74565add9d7df94e

--- frontend/overmountain/src/components/mountain/ReviewItems.tsx
+++ frontend/overmountain/src/components/mountain/ReviewItems.tsx
@@ -1,7 +1,106 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAppSelector, useAppDispatch } from '@app/hooks';
+import {
+  review,
+  getReviewList,
+  writeReview,
+  deleteReview,
+} from '@features/mountain/reviewSlice';
 
-function ReviewItems() {
-  return <></>;
+function ReviewItems(props: { mountainSeq: string }) {
+  const { mountainSeq } = props;
+
+  // 후기 리스트 받아오기
+  const reviewList = useAppSelector(review);
+  const getReviewDispatch = useAppDispatch();
+  useEffect(() => {
+    getReviewDispatch(getReviewList(mountainSeq));
+  }, [mountainSeq]);
+
+  // 후기 작성하기 (mountainSeq, content, rate)
+  const writeReviewDispatch = useAppDispatch();
+  const [content, setContent] = useState('');
+  const [rate, setRate] = useState(0);
+  const writeMyReview = () => {
+    if (content === '') {
+      alert('내용을 작성해주세요!');
+    } else if (rate === 0) {
+      alert('평점을 선택해주세요!');
+    } else {
+      const sendReview = {
+        mountainSeq: mountainSeq,
+        userSeq: 0,
+        content: content,
+        rate: rate,
+      };
+      writeReviewDispatch(writeReview(sendReview));
+    }
+  };
+
+  // 후기 삭제하기
+  const deleteReviewDistpatch = useAppDispatch();
+  const deleteMyReview = (reviewNum: number) => {
+    console.log(reviewNum);
+  };
+
+  return (
+    <>
+      {/* 후기 리스트 */}
+      <div>후기 및 평점</div>
+      {reviewList &&
+        reviewList.reviewList.map((oneReview) => (
+          <div>
+            <div>작성자 : {oneReview.nickname}</div>
+            <div>
+              프로필 이미지:
+              <img src={oneReview.profileImage} alt="프로필 이미지" />
+            </div>
+            <div>평점 : {oneReview.rate}</div>
+            <div>날짜 : {oneReview.createdAt}</div>
+            <div>내용 : {oneReview.content}</div>
+            {oneReview.writer ? (
+              <button
+                onClick={() => deleteMyReview(oneReview.reviewSeq)}
+                onKeyDown={() => deleteMyReview(oneReview.reviewSeq)}
+                type="button"
+              >
+                삭제하기버튼
+              </button>
+            ) : null}
+          </div>
+        ))}
+
+      {/* 후기 작성 */}
+      <div>
+        <form>
+          <label>Blog title:</label>
+          <input
+            type="text"
+            required
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            onKeyDown={(e) => setContent(e.currentTarget.value)}
+          />
+
+          <div
+            onClick={writeMyReview}
+            onKeyDown={writeMyReview}
+            role="presentation"
+          >
+            작성하기
+          </div>
+        </form>
+
+        <select onChange={(e) => setRate(Number(e.target.value))} value={rate}>
+          <option value="1">0.1톤</option>
+          <option value="2">0.2톤</option>
+          <option value="3">0.3톤</option>
+          <option value="4">0.4톤</option>
+          <option value="5">0.5톤</option>
+        </select>
+      </div>
+    </>
+  );
 }
 
 export default ReviewItems;
---
+++ frontend/overmountain/src/features/mountain/reviewSlice.ts
@@ -0,0 +1,93 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { baseURL } from '@features/port';
+import { RootState } from '@app/store';
+
+const initialReviewListState: ReviewList = {
+  reviewList: [],
+};
+
+// API 명세서 23번. 후기 리스트 GET
+export const getReviewList = createAsyncThunk(
+  'reviewSlice/getReviewList',
+  async (mountainIdx: string) => {
+    const url = `${baseURL}mountain/review/${mountainIdx}`;
+    const response = await axios({
+      method: 'GET',
+      headers: { Authorization: localStorage.getItem('token') },
+      url: url,
+    });
+    return response.data.result;
+  },
+);
+
+// API 명세서 24. 후기 작성 POST
+export const writeReview = createAsyncThunk(
+  'reviewSlice/writeReview',
+  async (review: {
+    mountainSeq: string;
+    userSeq: number;
+    content: string;
+    rate: number;
+  }) => {
+    const url = `${baseURL}mountain/review`;
+    const response = await axios({
+      method: 'POST',
+      headers: { Authorization: localStorage.getItem('token') },
+      data: review,
+      url: url,
+    });
+    return response.data.result;
+  },
+);
+
+// API 명세서 25. 후기 삭제 DELETE
+export const deleteReview = createAsyncThunk(
+  'reviewSlice/deleteReview',
+  async (reviewIdx: number) => {
+    const url = `${baseURL}mountain/review/${reviewIdx}`;
+    const response = await axios({
+      method: 'DELETE',
+      headers: { Authorization: localStorage.getItem('token') },
+      url: url,
+    });
+    return response.data.result;
+  },
+);
+
+export const reviewSlice = createSlice({
+  name: 'reviewSlice',
+  initialState: initialReviewListState,
+  reducers: {},
+  extraReducers: (builder) => {
+    // API 명세서 23번. 후기 리스트 GET
+    builder.addCase(getReviewList.fulfilled, (state, action) => {
+      state.reviewList = action.payload;
+      console.log('23 성공!', state.reviewList);
+    });
+    builder.addCase(getReviewList.rejected, (state, action) => {
+      console.log('23 실패!', action.error);
+    });
+
+    // API 명세서 24. 후기 작성 POST
+    builder.addCase(writeReview.fulfilled, (state, action) => {
+      console.log('24 성공!');
+    });
+    builder.addCase(writeReview.rejected, (state, action) => {
+      console.log('24 실패!', action.error);
+    });
+
+    // API 명세서 25. 후기 삭제 DELETE
+    builder.addCase(deleteReview.fulfilled, (state, action) => {
+      console.log('25 성공!');
+    });
+    builder.addCase(deleteReview.rejected, (state, action) => {
+      console.log('25 실패!', action.error);
+      alert('유효하지 않은 사용자입니다.');
+    });
+  },
+});
+
+export const review = (state: RootState) => state.reviews;
+
+export default reviewSlice.reducer;
