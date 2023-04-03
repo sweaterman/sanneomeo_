96131e2988d6232f9a42dfbf2d5f07dfb319653c
yunsuhee <suheeyun97@gmail.com>
2023-04-03 14:12:49 +0900

    Fix: 수정
    Former-commit-id: 19786934fdab6ade3dd02e949cbfa129cfe1dcd2

--- backend/flask/wsgi.py
+++ backend/flask/wsgi.py
@@ -54,132 +54,69 @@ def target_course():
 
     # 이거로 유사도 뽑고
     difficulty = data.get('level')
-    location = data.get('si')
+    location = data.get('region')
+    purpose = data.get('purpose')
     time = data.get('time')
 
 
     # 입력으로 들어온 문제 정보
-    difficulty = '쉬움'
+    difficulty = 1
     location = '서울'
+    purpose = 1
     time = 30
 
-    # 입력된 난이도를 범위 값으로 변환합니다.
-    difficulty_ranges = {
-        '쉬움': [1, 1.3],
-        '중간': [1.3, 2.0],
-        '어려움': [2.0, 3.0]
-    }
-    for key, value in difficulty_ranges.items():
-        if value[0] <= difficulty <= value[1]:
-            difficulty = key
-            break
-
-    # 데이터베이스에서 모든 문제 정보를 가져옵니다.
-    conn = sqlite3.connect('problems.db')
-    cur = conn.cursor()
-    cur.execute("SELECT difficulty, location, time FROM problems")
-    problems = cur.fetchall()
-    conn.close()
-
-    # 각 문제의 난이도, 지역, 시간 정보를 벡터로 변환합니다.
-    def convert_difficulty_to_range(difficulty):
-        for key, value in difficulty_ranges.items():
-            if value[0] <= difficulty <= value[1]:
-                return key
-        return None
-
-    vectors = []
-    for problem in problems:
-        problem_difficulty = convert_difficulty_to_range(problem[0])
-        problem_location = problem[1]
-        problem_time = problem[2]
-        vectors.append(np.array([problem_difficulty, problem_location, problem_time]))
-
-    # 입력된 문제 정보도 벡터로 변환합니다.
-    new_problem = np.array([difficulty, location, time])
-    new_problem[0] = convert_difficulty_to_range(new_problem[0])
-
-    # 각 문제와 새로운 문제 간의 코사인 유사도를 계산합니다.
-    similarities = []
-    for i, vector in enumerate(vectors):
-        if vector[1] == new_problem[1]:
-            # 지역이 같을 경우, 난이도와 시간 정보만을 이용하여 유사도를 계산합니다.
-            weight = np.array([1, 0, 1])
-            problem_difficulty = difficulty_ranges[vector[0]]
-            new_problem_difficulty = difficulty_ranges[new_problem[0]]
-            vector[0] = np.mean(problem_difficulty)
-            new_problem[0] = np.mean(new_problem_difficulty)
-            sim = cosine_similarity(vector[[0, 2]].reshape(1, -1), new_problem[[0, 2]].reshape(1, -1))
-            similarities.append((i, sim[0][0]))
-        else:
-            # 지역이 다를 경우, 모든 정보를 이용하여 유사도를 계산합니다.
-            weight = np.array([1, 1, 1])
-            sim = cosine_similarity(vector.reshape(1, -1), new_problem.reshape(1, -1))
-            similarities.append((i, sim[0][0]))
-
-    # 유사도가 가장 높은 문제의 지역 정보를 반환합니다.
-    most_similar = max(similarities, key=lambda x: x[1])
-    location = vectors[most_similar[0]][1]
-    print
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    #유사도 뽑기
+    # 코스 정보 가져오기
     curs = mydb.cursor()
-    sql = "select c.course_seq, c.mountain_seq,  c.difficulty_mean, c.time, m.gu, m.dong  from tbl_course c left join tbl_mountain m on c.mountain_seq = m.mountain_seq"
+    sql = "select c.course_seq , c.mountain_seq, c.difficulty_mean, c.time, m.si from tbl_course c left join tbl_mountain m on c.mountain_seq = m.mountain_seq"
     curs.execute(sql)
     result = curs.fetchall()
     mydb.close()
 
+    # 데이터프레임 생성 및 필터링
     df = pd.DataFrame(result)
-    df.columns = ['course_seq', 'mountain_seq', 'difficulty_mean', 'time', 'gu', 'dong']
-
-    features = ['difficulty_mean', 'time', 'gu','dong']
-    # 정규화
-    scaler = MinMaxScaler()
-    df[features] = scaler.fit_transform(df[features])
-
-    X = df[features].values
-
-    target_trail = {feature: X[0, i] for i, feature in enumerate(features)}
-
-    y = np.array([[target_trail[feature] for feature in features]])
-
-    distances = euclidean_distances(X, y)
-
-    ranked_trails = sorted(zip(df['course_seq'], distances), key=lambda x: x[1])
-    recommended_trails = [trail[0] for trail in ranked_trails if trail[0] != target_trail['course_seq']][:10]
-
-    recommended_result = {"recommended_result": recommended_trails}
-
-    print(recommended_result)
+    df.columns = ['course_seq', 'mountain_seq', 'difficulty_mean', 'time', 'si']
 
 
+    # 난이도 필터링
+    if difficulty == 1 : #쉬움
+        df = df[df['difficulty_mean'] <= 1.0]
+    elif difficulty == 2: #중간
+        df = df[(df['difficulty_mean'] > 1.0) & (df['difficulty_mean'] < 1.3)]
+    elif difficulty == 3: #어려움
+        df = df[(df['difficulty_mean'] >= 1.3) ]
+
+    print(df)
+
+    # 지역 필터링
+    df = df[df['si'].str.contains(location)]
+    print(df)
+
+    # 시간 필터링
+    if time == 1: # 30분 미만
+        df = df[df['time'] < 30]
+    elif time == 2: # 30분 이상 1시간 미만
+        df = df[(df['time'] >= 30 ) & (df['time'] < 60)]
+    elif time == 3: # 1시간 이상 2시간 미만
+        df = df[(df['time'] >= 60) & (df['time'] < 120)]
+    elif time == 4: # 2시간 이상 3시간 미만
+        df = df[(df['time'] >= 120) & (df['time'] < 180)]
+    elif time == 5: # 3시간 이상
+        df = df[(df['time'] >= 180) ]
+
+    # 목적 필터링
+    # df['sum'] = df['difficulty_mean'] + df['time']
+    # if purpose == 1 : #힐링
+    #     df = df.loc[df['sum'].idxmin(), ['difficulty_mean', 'time']]
+    # elif purpose == 2 : #도전
+    #     df['sum'] = df['difficulty_mean'] + df['time']
+    #     df = df.loc[df['sum'].idxmax()]
+
+    print("목적 필터링")
+    print(df)
 
 
 
-    # 시간 기준으로 필터링
-
-
-    # 집주변 필터링
-
-    # 리스트 중에 힐링이면 유사도 제일 높은거 도전이면 유사도 낮은거 -> 하나 결과로 return
-
-
-    return "성공"
+    return "안녕"
 
 if __name__ == '__main__':
     app.run(debug=True)
