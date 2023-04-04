9b1da301e71c2571bb18b5d8484d068c63b39e96
이연희 <leeuh7262@naver.com>
2023-04-04 14:22:23 +0900

    Feat: 코드 리팩토링 및 필터링 적용
    Former-commit-id: d00687fe6f25891b0573c3293e8f2cdd02d1a5a2

--- frontend/overmountain/src/components/user/ChallengeItems.tsx
+++ frontend/overmountain/src/components/user/ChallengeItems.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import difficulty from '@assets/images/mountain_selected.svg';
-import undifficulty from '@assets/images/mountain_unselected.svg';
-import MainRamgi from '@assets/images/flagramgi.png';
+import mountain_selected from '@assets/images/mountain_selected.png';
+import mountain_unselected from '@assets/images/mountain_unselected.png';
+import MainRamgi from '@assets/images/ramgi_flag.png';
+import { NavLink } from 'react-router-dom';
 
 function ChallengeItems(props: { mountain: Mountain }) {
+  // eslint-disable-next-line react/destructuring-assignment
   const { mountain } = props.mountain;
 
   let mountainName = mountain.name;
@@ -12,26 +14,53 @@ function ChallengeItems(props: { mountain: Mountain }) {
   return (
     <div className="listbox" key={mountain.mountainSeq}>
       <div className="mountain-container">
-        <div className="mountain-photo">
-          <img src={mountain.photo} alt="list" className="mountain-100-photo" />
-        </div>
+        <NavLink to={`/mountain/detail/${mountain.mountainSeq}`}>
+          <div className="mountain-photo">
+            <img
+              src={mountain.photo}
+              alt="list"
+              className="mountain-100-photo"
+            />
+          </div>
+        </NavLink>
+
         <div className="textbox">
           <div className="textbox-name">
-            <div className="mountain-name">{name}</div>
+            <NavLink to={`/mountain/detail/${mountain.mountainSeq}`}>
+              <div className="mountain-name">{name}</div>
+            </NavLink>
+
             <div className="height">{Math.floor(mountain.altitude)}m</div>
           </div>
           <div className="address">
             {mountain.si} {mountain.gu} {mountain.dong}
           </div>
           <div className="difficulty">
-            <img src={difficulty} alt="difficulty" />
-            <img src={difficulty} alt="difficulty" />
-            <img src={undifficulty} alt="undifficulty" />
+            {mountain.difficulty === '어려움' ? (
+              <div className="difficulty">
+                <img src={mountain_selected} alt="difficulty" />
+                <img src={mountain_selected} alt="difficulty" />
+                <img src={mountain_selected} alt="difficulty" />
+              </div>
+            ) : null}
+            {mountain.difficulty === '중간' ? (
+              <div className="difficulty">
+                <img src={mountain_selected} alt="difficulty" />
+                <img src={mountain_selected} alt="difficulty" />
+                <img src={mountain_unselected} alt="difficulty" />
+              </div>
+            ) : null}
+            {mountain.difficulty === '쉬움' ? (
+              <div className="difficulty">
+                <img src={mountain_selected} alt="difficulty" />
+                <img src={mountain_unselected} alt="difficulty" />
+                <img src={mountain_unselected} alt="difficulty" />
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
-      
-      
+
       <div
         className={
           mountain.conquer ? 'conquer-mascott' : 'conquer-mascott not-conquered'
